c7582e84345a51e96c80913d05fb06c84bc175f6
Tanner Armstrong <tjaxgd@mst.edu>
2022-04-10 02:29:32 -0500

    updated booth.py

--- booth.py
+++ booth.py
@@ -6,92 +6,117 @@
 
 
 import sys
-
+import time
 
 # Function to perform Booth's algorithm
-# Accepts as input a multiplier and multiplicand
+# Accepts as input a multiplicand and multiplier
 # Returns the product of the two numbers
-def booth(multiplier, multiplicand):
-    n = len(multiplier)                 # n is the number of bits being used
+def booth(multiplicand, multiplier):
+    n = len(multiplicand)                 # n is the number of bits being used
     qn1 = '0'                           # qn+1 is set to 0
     ac = '0' * n                        # the ac starts as 0s
     counter = n                         # counter for how many times to loop through the alogrithm
+    num_additions = 0
+    num_subtractions = 0
 
-    print("Initial\t\t", ac, multiplier, '0')
+    print("    AC   Q    Qn+1")
+    print(f"    {ac} {multiplicand} 0   |  Initial value")
 
+    start_time = time.time()
     while True:
-        qn = multiplier[n-1]    # get the last bit of the multipler, q
+        qn = multiplicand[n-1]    # get the last bit of the multipler, q
 
-        # If the last bit of the multiplier, q, and qn+1 are 10, subtract the multipler from the accumulator
+        # If the last bit of the multiplicand, q, and qn+1 are 10, subtract the multipler from the accumulator
         if (qn, qn1) == ('1', '0'):
-            ac = bin(int(ac, 2) + int(twos_comp(multiplicand), 2))[2:].zfill(n)
+            ac = bin(int(ac, 2) + int(twos_comp(multiplier), 2))[2:].zfill(n)
             if len(ac) > n: ac = ac[len(ac)-n:]
-            print("Subtract\t", ac, multiplier, qn1)
+            print(f"    {ac} {multiplicand} {qn1}   |  Subtract multiplier")
+            num_subtractions = num_subtractions + 1
 
-        # Else if the last bit of the multiplier, q, and qn+1 are 01, add the multiplier to the accumulator
+        # Else if the last bit of the multiplicand, q, and qn+1 are 01, add the multiplicand to the accumulator
         elif (qn, qn1) == ('0', '1'):
-            ac = bin(int(ac, 2) + int(multiplicand, 2))[2:]
+            ac = bin(int(ac, 2) + int(multiplier, 2))[2:]
             if len(ac) > n: ac = ac[len(ac)-n:]
-            print("Add\t\t", ac, multiplier, qn1)
+            print(f"    {ac} {multiplicand} {qn1}   |  Add multiplier")
+            num_additions = num_additions + 1
         
-        # Perform an arithmetic right shift on the contents of the accumulator, multiplier, and qn+1
-        ac, multiplier, qn1 = ars(ac, multiplier, qn1)
-        print("Shift\t\t", ac, multiplier, qn1)
+        # Perform an arithmetic right shift on the contents of the accumulator, multiplicand, and qn+1
+        ac, multiplicand, qn1 = ars(ac, multiplicand, qn1)
+        print(f"    {ac} {multiplicand} {qn1}   |  Shift right")
 
         # Decrement the counter
         counter = counter - 1
 
         # If the counter reaches zero, break out of the loop
         if counter == 0:
+            end_time = time.time()
             break
     
+    print(f" Answer: \t{ac + multiplicand}\n\n Excec time: \t{end_time - start_time} seconds\n Iterations: \t{n}\n Additions: \t{num_additions}\n Subtractions: \t{num_subtractions}")
     # Return the contents of the accumulator and the multipler as a single string
-    return (ac + multiplier)
+    return (ac + multiplicand)
 
 # Function to perform modified Booth's algorithm
-# Accepts as input a multiplier and multiplicand
+# Accepts as input a multiplicand and multiplier
 # Returns the product of the two numbers
-def modified_booth(multiplier, multiplicand):
+def modified_booth(multiplicand, multiplier):
     # Initilizations
-    twos = twos_comp(multiplicand)  # get the two's compliment of the multiplicand
-    padded = multiplier + "0"       # pad the muliplier to get recode results
-    orglen = len(multiplier)        # get the original length for filling
+    twos = twos_comp(multiplier)  # get the two's compliment of the multiplier
+    padded = multiplicand + "0"       # pad the muliplier to get recode results
+    orglen = len(multiplicand)        # get the original length for filling
     maxlen = orglen * 2             # get max length for filling
     recode = []                     # empty list for recode results and dictionary for finding values
     recode_vals = {"001":1, "010":1, "011":2, "100":-2, "101":-1, "110":-1}
     result = 0                      # final result is currently 0
 
+    start_time = time.time()
     # find the recode results for each set of 3 bits
     for i in range(0, len(padded), 2):
         if padded[i:i+3] in recode_vals:
             recode.insert(0, recode_vals.get(padded[i:i+3]))
     
+    temp = ""
+    for c in range(len(recode)-1, -1, -1):
+        temp += " " + str(recode[c])
+    print(f"  Bit Recoding: {recode}\n    {multiplicand}\n  x{temp}\n--------------------------")
+
     # perform the calculations using the recode results
     for c in range(len(recode)):
         # pad the end for each iteration
         if c > 0: 
             twos += "00"
-            muliplicand += "00"
+            multiplier += "00"
             orglen += 2
 
         # perform the calcualtions
-        if recode[c] == 1: recode[c] = bin(int(multiplicand, 2))[2:].zfill(orglen)
-        if recode[c] == 2: recode[c] = bin((2 * int(multiplicand, 2)))[2:].zfill(orglen)
+        if recode[c] == 1: recode[c] = bin(int(multiplier, 2))[2:].zfill(orglen)
+        if recode[c] == 2: recode[c] = bin((2 * int(multiplier, 2)))[2:].zfill(orglen)
         if recode[c] == -2: recode[c] = bin((2 * int(twos, 2)))[2:].zfill(orglen)
         if recode[c] == -1: recode[c] = bin(int(twos, 2))[2:].zfill(orglen)
 
-        # pad the beginning of each calcualtion 
+        # pad the beginning of each calcualtion
         recode[c] = recode[c].rjust(maxlen, recode[c][0])
 
-    # add all the results together to get final result
-    for r in recode:
-        result += int(r, 2)
+        # add to results
+        result += int(recode[c], 2)
 
-    return result
+    for r in range(len(recode)-1):
+        print(f"    {recode[r]}")
+    print(f"  + {recode[len(recode)-1]}\n--------------------------")
+    
+    if len(bin(result)[2:]) > maxlen:
+        result = bin(result)[len(bin(result)[2:])-maxlen+2:]
+        print(f" Answer: {bin(int(result, 2))[2:].zfill(maxlen)}")
+        return bin(int(result, 2))[2:].zfill(maxlen)
+
+    result = bin(result)
+    end_time = time.time()
+    print(f" Answer: {bin(int(result, 2))[2:].zfill(maxlen)}")
+    return bin(int(result, 2))[2:].zfill(maxlen)
 
 
 # Function to perform the arithmetic right shift
-# Accepts as input the accumulator (ac), the multiplier (mult), and qn+1 (qn1)
+# Accepts as input the accumulator (ac), the multiplicand (mult), and qn+1 (qn1)
 # Returns ac, mult, and qn1 post shift as a three tuple
 def ars(ac, mult, qn1):
     combined = ac + mult + qn1
@@ -112,22 +137,25 @@ def twos_comp(num):
 
 
 if __name__ == "__main__":
-    multiplicand = str(sys.argv[1])
-    multiplier = str(sys.argv[2])
+    multiplier = str(sys.argv[1])
+    multiplicand = str(sys.argv[2])
 
-    booth_product = booth(multiplier, multiplicand)
-    modified_product = modified_booth(multiplier, multiplicand)
+    print("\n === Booth's Algorithm ===")
+    booth_product = booth(multiplicand, multiplier)
 
     # If the most significant print of the product is a 1, this indicates the answer is negative
     # Therefore, the product is set to the two's compliment translation of the product
     if booth_product[0] == '1':
         booth_product = twos_comp(booth_product)
-        print(f"Product = {booth_product}, -{int(booth_product, 2)}")
-    else: print(f"Product = {booth_product}, {int(booth_product, 2)}")
+        print(f"\n Booth's Product:\n  binary:\t{booth_product}\n  decimal:\t-{int(booth_product, 2)}")
+    else: print(f"\n Booth's Product:\n  binary:\t{booth_product}\n  decimal:\t{int(booth_product, 2)}")
+
+    print("\n\n === Modified Booth's Algorithm ===")
+    modified_product = modified_booth(multiplicand, multiplier)
 
     # If the most significant print of the product is a 1, this indicates the answer is negative
     # Therefore, the product is set to the two's compliment translation of the product
     if modified_product[0] == '1':
         modified_product = twos_comp(modified_product)
-        print(f"Product = {modified_product}, -{int(modified_product, 2)}")
-    else: print(f"Product = {modified_product}, {int(modified_product, 2)}")
+        print(f"\n Modified Booth's Product:\n  binary:\t{modified_product}\n  decimal:\t-{int(modified_product, 2)}")
+    else: print(f"\n Modiefied Booth's Product:\n  binary:\t{modified_product}\n  decimal:\t{int(modified_product, 2)}")
